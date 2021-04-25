6a8d92da6f4088f100298cb0d71c9dab57b7c8d5
vijai <vramachandran@medline.com>
2021-04-25 11:11:52 -0500

    Logo added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">React Helloworld</header>
+      <img width={500} height={250} src={logo} />
+      <br></br>
+      <img width={500} height={250} src={logo} />
+      <img width={500} height={250} src='https://s.cdpn.io/3/kiwi.svg' />
+      <img width={500} height={250} src={logo} />
+      <br></br>
+      <img width={500} height={250} src={logo} />
+
+      <img width={500} height={250} src={logo} />
+
+
+
     </div>
   );
 }
 
 export default App;
+
+
